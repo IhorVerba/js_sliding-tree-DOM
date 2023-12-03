2c1e90317e2db2ad4c82a5d3d932a2bcc2b8771b
IhorVerba <ihor.verbivskyi@gmail.com>
2023-12-03 17:01:43 +0200

    make build

---
+++ index.527fd56f.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");e.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");e.prepend(t),t.append(t.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var t=e.target.parentNode.querySelector("ul");t&&(t.hidden=!t.hidden)}});//# sourceMappingURL=index.527fd56f.js.map
+
+//# sourceMappingURL=index.527fd56f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const tree = document.querySelector(".tree");
+tree.querySelectorAll("li").forEach((li)=>{
+    const span = document.createElement("span");
+    li.prepend(span);
+    span.append(span.nextSibling);
+});
+tree.addEventListener("click", function(e) {
+    if (e.target.tagName !== "SPAN") return;
+    const childrenContainer = e.target.parentNode.querySelector("ul");
+    if (!childrenContainer) return;
+    childrenContainer.hidden = !childrenContainer.hidden;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
